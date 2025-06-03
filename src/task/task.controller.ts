d2a5ab41bb77e249d23b756228e089ea75eedd38
ChekushkinPM <x-chekushkin-pavel@yandex.ru>
2025-06-03 20:28:13 +0400

    nest g res task --no-spec

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TaskService } from './task.service';
 
 @Controller('task')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
+
+  @Get()
+  public findAll() {
+    return [
+      {
+        id: 1,
+        title: 'Task 1',
+        isCompleted: false,
+      },
+      {
+        id: 2,
+        title: 'Task 2',
+        isCompleted: true,
+      },
+    ];
+  }
 }
