fde4771d343e4a7ff18a44b39ea64667f5cb2fe5
ChekushkinPM <x-chekushkin-pavel@yandex.ru>
2025-06-03 21:55:14 +0400

    add validation

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,3 +1,37 @@
+import {
+  IsArray,
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsPositive,
+  IsString,
+  Length,
+} from 'class-validator';
+
+export enum TaskTag {
+  WORK = 'work',
+  STUDY = 'study',
+  HOME = 'home',
+}
+
 export class CreateTaskDto {
+  @IsString({ message: 'Название задачи должно быть строкой' })
+  @IsNotEmpty({ message: 'Название задачи не может быть пустым' })
+  @Length(2, 40, { message: 'Название должно быть от 2 до 40 символов' })
   readonly title: string;
+
+  @IsString({ message: 'Название задачи должно быть строкой' })
+  @IsOptional()
+  description: string;
+
+  @IsInt({ message: 'Приоритет должен быть целым числом' })
+  @IsPositive({ message: 'Приоритет должен быть числом' })
+  @IsOptional()
+  priority: number;
+
+  @IsArray({ message: 'теги должны быть массивом' })
+  @IsEnum(TaskTag, { each: true, message: 'Недопустимое значение тега' })
+  @IsOptional()
+  tags: string[];
 }
